4da120c02126a4a2ac6c524a290f94539a9d59af
Stef Spears <spears.sj87@gmail.com>
2019-09-02 20:31:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-require 'pry'
+#require 'pry'
 
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
@@ -43,4 +43,4 @@ shipping_manifest["gun powder"] = 4
   # return the shipping_manifest hash below
 shipping_manifest
 end
-binding.pry
+#binding.pry
