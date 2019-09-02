828ad2fd00b32748bcc23d6e3af8474196d6fb03
Stef Spears <spears.sj87@gmail.com>
2019-09-02 20:26:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -39,5 +39,5 @@ shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-return shipping_manifest
+shipping_manifest["oil paintings"]
 end
